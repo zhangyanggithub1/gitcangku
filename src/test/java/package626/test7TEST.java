2c8c84e78e1c476fc74920e7db669b5ec5752788
Machenike <15510356501@163.com>
2022-07-27 15:31:23 +0800

    第二次pull

--- src/test/java/package626/test7TEST.java
+++ src/test/java/package626/test7TEST.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class test7TEST {
     @Test(dependsOnMethods = "fangfa2",alwaysRun = true)
     public void fangfa(){
-        System.out.println("fangfa");
+        System.out.println("fangfa111");
         // Assert.assertEquals(false,true);
     }
     @Test
