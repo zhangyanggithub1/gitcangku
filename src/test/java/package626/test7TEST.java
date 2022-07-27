db732ef05bd2249b961711fcd3917e43d9587b12
Machenike <15510356501@163.com>
2022-07-27 15:17:54 +0800

    第二次pull

---
+++ src/test/java/package626/test7TEST.java
@@ -0,0 +1,16 @@
+package package626;
+
+import org.testng.annotations.Test;
+
+public class test7TEST {
+    @Test(dependsOnMethods = "fangfa2",alwaysRun = true)
+    public void fangfa(){
+        System.out.println("fangfa");
+        // Assert.assertEquals(false,true);
+    }
+    @Test
+    public void fangfa2(){
+        System.out.println("fangfa2");
+        // Assert.assertEquals(false,true);
+    }
+}
