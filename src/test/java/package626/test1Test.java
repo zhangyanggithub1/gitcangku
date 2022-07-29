a8a30e648b4b56dc7c3dda94807a588084c62690
aa <15510356501@163.COM>
2022-07-29 14:18:16 +0800

    第三次次更新

--- src/test/java/package626/test1Test.java
+++ src/test/java/package626/test1Test.java
@@ -11,7 +11,7 @@ public class test1Test {
      System.out.println("增加一行代码");
      System.out.println("增加一行代码");
      System.out.println("增加一行代码");
-     System.out.println("增加一行代码");
+     System.out.println("增加一行1代码");
  }
     @Test
     public void fangfa2(){
