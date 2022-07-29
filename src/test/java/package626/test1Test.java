cd125f7c7c957271341834257103824eb3f6eafd
aa <15510356501@163.COM>
2022-07-29 11:46:40 +0800

    第一次更新

--- src/test/java/package626/test1Test.java
+++ src/test/java/package626/test1Test.java
@@ -8,6 +8,8 @@ public class test1Test {
      System.out.println("fangfa");
     // Assert.assertEquals(false,true);
      System.out.println("增加一行代码");
+     System.out.println("增加一行代码");
+     System.out.println("增加一行代码");
  }
     @Test
     public void fangfa2(){
